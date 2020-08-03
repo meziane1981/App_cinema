89f082d98ba3a39375cc8b2ab13a5583288db1f7
William Paparone <willpaparone@gmail.com>
2020-08-03 15:05:56 +0200

    Initial Commit

---
+++ App_cinema/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App_cinema
+//
+//  Created by William Paparone on 03/08/2020.
+//  Copyright © 2020 William Paparone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
